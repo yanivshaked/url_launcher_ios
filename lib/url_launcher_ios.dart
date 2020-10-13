e70a319c44ef355e85d428e1186b9939a189f41e
Yaniv Shaked <shakedyaniv@gmail.com>
2020-10-13 12:47:07 +0300

    [Fix] Several fixes

--- lib/url_launcher_ios.dart
+++ lib/url_launcher_ios.dart
@@ -1,42 +1,73 @@
 import 'dart:async';
 
 import 'package:flutter/services.dart';
+import 'package:flutter/widgets.dart';
 import 'package:meta/meta.dart' show required;
 
 class UrlLauncherIos {
   static const MethodChannel _channel = const MethodChannel('url_launcher_ios');
 
-  Future<bool> canLaunch(String url) {
-    return _channel.invokeMethod<bool>(
+  Future<bool> canLaunch(String url) async {
+    if (url == null) {
+      return false;
+    }
+    return await _channel.invokeMethod<bool>(
       'canLaunch',
       <String, Object>{'url': url},
     );
   }
 
-  Future<void> closeWebView() {
-    return _channel.invokeMethod<void>('closeWebView');
+  Future<void> closeWebView() async {
+    return await _channel.invokeMethod<void>('closeWebView');
   }
 
   Future<bool> launch(
-    String url, {
-    @required bool useSafariVC,
-    @required bool useWebView,
-    @required bool enableJavaScript,
-    @required bool enableDomStorage,
-    @required bool universalLinksOnly,
-    @required Map<String, String> headers,
-  }) {
-    return _channel.invokeMethod<bool>(
+    String urlString, {
+    bool forceSafariVC,
+    bool forceWebView,
+    bool enableJavaScript,
+    bool enableDomStorage,
+    bool universalLinksOnly,
+    Map<String, String> headers,
+    Brightness statusBarBrightness,
+    String webOnlyWindowName,
+  }) async {
+    assert(urlString != null);
+    final Uri url = Uri.parse(urlString.trimLeft());
+    final bool isWebURL = url.scheme == 'http' || url.scheme == 'https';
+    if ((forceSafariVC == true || forceWebView == true) && !isWebURL) {
+      throw PlatformException(
+          code: 'NOT_A_WEB_SCHEME',
+          message: 'To use webview or safariVC, you need to pass'
+              'in a web URL. This $urlString is not a web URL.');
+    }
+
+    /// [true] so that ui is automatically computed if [statusBarBrightness] is set.
+    bool previousAutomaticSystemUiAdjustment = true;
+    if (statusBarBrightness != null) {
+      previousAutomaticSystemUiAdjustment = WidgetsBinding.instance.renderView.automaticSystemUiAdjustment;
+      WidgetsBinding.instance.renderView.automaticSystemUiAdjustment = false;
+      SystemChrome.setSystemUIOverlayStyle(
+          statusBarBrightness == Brightness.light ? SystemUiOverlayStyle.dark : SystemUiOverlayStyle.light);
+    }
+    final bool result = await _channel.invokeMethod<bool>(
       'launch',
       <String, Object>{
-        'url': url,
-        'useSafariVC': useSafariVC,
-        'useWebView': useWebView,
-        'enableJavaScript': enableJavaScript,
-        'enableDomStorage': enableDomStorage,
-        'universalLinksOnly': universalLinksOnly,
-        'headers': headers,
+        'url': urlString,
+        'useSafariVC': forceSafariVC ?? true,
+        'useWebView': forceWebView ?? false,
+        'enableJavaScript': enableJavaScript ?? false,
+        'enableDomStorage': enableDomStorage ?? false,
+        'universalLinksOnly': universalLinksOnly ?? false,
+        'headers': headers ?? <String, String>{},
       },
     );
+
+    assert(previousAutomaticSystemUiAdjustment != null);
+    if (statusBarBrightness != null) {
+      WidgetsBinding.instance.renderView.automaticSystemUiAdjustment = previousAutomaticSystemUiAdjustment;
+    }
+
+    return result;
   }
 }
